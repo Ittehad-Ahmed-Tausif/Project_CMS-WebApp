86901c2a054725ec469fbce2bd58c799ce6c7136
Ittehad Ahmed Tausif <146110898+Ittehad-Ahmed-Tausif@users.noreply.github.com>
2024-10-13 22:04:00 +0600

    Add files via upload

---
+++ pages/1_Contacts Us.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.title('Contact Us')
+st.text('')
+st.text('')
+st.markdown('#### About the Developer ')
+# st.text('')
+st.write("""
+
+
+This application was developed by **Ittehad Ahmed Tausif**. As a passionate software developer, Ittehad strives to create solutions that are not only functional but also user-friendly. With a strong foundation in programming and a commitment to continuous learning, Ittehad has developed this application to meet the specific needs of its users, ensuring a smooth and seamless experience.
+
+For inquiries or feedback, feel free to reach out and share your thoughts. Ittehad values user input and is dedicated to improving the application based on real user experiences.
+""")
+
+st.text('')
+
+exp = Warning("This site is currently under construction. Some features may not be available.")
+st.exception(exp)
