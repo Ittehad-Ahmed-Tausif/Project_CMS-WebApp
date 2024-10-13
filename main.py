4f24882163a272b6698311b66e1a41f9d07052c6
Ittehad Ahmed Tausif <146110898+Ittehad-Ahmed-Tausif@users.noreply.github.com>
2024-10-13 12:23:48 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import streamlit as st
 
-st.title('Hello, World!')
+# Title of the app
+st.title("Login or Signup")
+
+# Input fields for username and password
+username = st.text_input("Username")
+password = st.text_input("Password", type="password")
+
+# Buttons for login and signup
+if st.button("Login"):
+    st.write("Login button clicked")
+elif st.button("Signup"):
+    st.write("Signup button clicked")
